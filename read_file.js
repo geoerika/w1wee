86c98ac716d77414894b045d6d940ed5f0e9a110
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-01 01:54:16 +0000

    some minor changes to read_file.js

--- read_file.js
+++ read_file.js
@@ -2,21 +2,21 @@ var fs = require("fs");
 
 var filePath = process.argv[2];
 
-console.log("Going to open an existing file");
+// console.log("Going to open an existing file");
 fs.open(filePath, 'r+', function(err, fd) {
 
   if (err) {
     return console.error(err);
   }
 
-  console.log("File opened successfully!");
-  console.log("Let's read newly written data");
+  // console.log("File opened successfully!");
+  // console.log("Read data");
 
-  fs.readFile(filePath, function (err, data) {
+  fs.readFile(filePath, function (err, fileData) {
     if (err) {
       return console.error(err);
     }
-    console.log("Asynchronous read: " + data.toString());
+    console.log("Asynchronous read from file" + filePath + ": " + fileData.toString());
   });
 });
 
